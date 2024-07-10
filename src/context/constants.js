4b08c2cdaad59a83f0c5aadd250d5e391eb11dd0
Deva <goravadevendra89@gmail.com>
2024-07-10 06:06:10 +0000

    solution

---
+++ src/context/constants.js
@@ -0,0 +1,20 @@
+export const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+export const apiUrl =
+  'https://run.mocky.io/v3/72562bef-1d10-4cf5-bd26-8b0c53460a8e'
+
+export const loginUrl = 'https://apis.ccbp.in/login'
+
+export const emptyCartImgUrl =
+  'https://assets.ccbp.in/frontend/react-js/nxt-trendz-empty-cart-img.png'
+
+export const failImgUrl =
+  'https://assets.ccbp.in/frontend/react-js/nxt-trendz/nxt-trendz-products-error-view.png'
+
+export const notFoundImgUrl =
+  'https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png'
